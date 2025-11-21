e119078c5cadf6971e6faeca2fa95da0864f9d9b
bhumikas202006-cyber <bhumikas202006@gmail.com>
2025-11-21 09:18:48 +0000

    Add files via upload

---
+++ e.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  9 05:55:39 2025
+
+@author: admin
+"""
+
+num=int(input("enter the num"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+    
+         
